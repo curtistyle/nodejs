475f7ed87d419dcc8362a712fa4983fdc3b0f6e1
Sebastian Maldonado <58012659+curtistyle@users.noreply.github.com>
2023-10-25 16:11:09 -0300

    update

--- glosario/main/main.js
+++ glosario/main/main.js
@@ -1,40 +1,49 @@
 var btn_agregar = document.getElementById('btn_agregar')
-btn_agregar.addEventListener("click", agregarPalabra)
-
-var in_data = {
-    palabra : '',
-    traduccion : '',
-    tipo : ''
-}
+btn_agregar.addEventListener("click", agregarPalabra() )
 
 var palabras = new Array()
 
 function createRow(){
     tr = document.createElement('tr')
-
-
+    
+    f = new FileReader()
+    
 }
 
-const fs = require('fs')
+import { readFile } from 'fs'
 
 
 function agregarPalabra(){
+    var in_data = {
+        palabra : '',
+        traduccion : '',
+        tipo : ''
+    }
+    
     in_data.palabra = document.getElementById('in_palabra').value
     in_data.traduccion = document.getElementById('in_traduccion').value
     in_data.tipo = document.getElementById('in_tipo').value
     
+    console.log("hola")
     
-    fs.readFile('datos.json', (err, data) => {
+    readFile('datos.json', (err, data) => {
         if (err) throw err;
-        let datos = JSON.parse(data);
-        console.log(datos)
-        datos.palabras.push(in_data)
-
-        fs.writeFile('datos.json', JSON.stringify(datos), (err) => {
-            if (err) throw err;
-            console.log('Archivo actualizado satisfactoriamente');
-        });
+        const datos = JSON.parse(data);
+        console.log(datos);
     });
+    
+    
+    // fs.readFile('datos.json', (err, data) => {
+        //     if (err) throw err;
+    //     let datos = JSON.parse(data);
+    //     console.log(datos)
+    //     datos.palabras.push(in_data)
+
+    //     fs.writeFile('datos.json', JSON.stringify(datos), (err) => {
+    //         if (err) throw err;
+    //         console.log('Archivo actualizado satisfactoriamente');
+    //     });
+    // });
 
 
 }
